a41ac11be8fe0ba377816317a98d0bc508a6fb5b
smalll3b <18568asd@gmail.com>
2026-01-04 13:55:44 +0800

    Initial Commit

---
+++ CampusApp/CampusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusAppApp.swift
+//  CampusApp
+//
+//  Created by smalll3b on 4/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct CampusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CampusApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CampusApp
+//
+//  Created by smalll3b on 4/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
